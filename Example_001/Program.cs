35f5bd907b0207cd4c0a2aede386baa4b5e502a3
Philipp K <philippkargapoloff@gmail.com>
2022-10-16 17:03:42 +0300

    добавлено решение по нахождению квадрата числа

---
+++ Example_001/Program.cs
@@ -0,0 +1,10 @@
+﻿// 1. Ввод числа
+Console.WriteLine("Программа вычисления квадрата числа");
+Console.WriteLine("Введите число:");
+
+string inputString = Console.ReadLine()!;
+int input = int.Parse(inputString);
+// 2. Возвести число в квадрат
+int result = input * input;
+//3. Вывод результата на консоль
+Console.WriteLine("Квардрат числа равен " + result);
