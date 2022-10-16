04c17005fc2f98c451ef81fc1589e8de32a6ef70
Philipp K <philippkargapoloff@gmail.com>
2022-10-16 18:20:56 +0300

    Задача на вывод диапазона от -n до n при вводе числа n

---
+++ Zadanie_005/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа, которая на вход принимает число N, на выходе выводит диапазон чисел от -N до N.
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine()!);
+int number_minus = number * (-1);
+
+if (number < 0)
+{
+    int temp = number;
+    number = number_minus;
+    number_minus = temp;
+}
+
+while (number_minus <= number)
+{
+    Console.Write(number_minus + " ");
+    number_minus++;
+}
